630d52e38cc30e611b0a1d3ced3eb91aebdc98fd
Warren Wong (Adecco) <a-wawon@microsoft.com>
2017-08-02 15:16:56 +0800

    test2

---
+++ www/www/js/controllers.js
@@ -0,0 +1,121 @@
+var app = angular.module('mshack', ['ionic', 'ionic-material']);
+angular.module('app.controllers', [])
+
+
+    .controller('cameraTabDefaultPageCtrl', ['$scope', '$stateParams', '$http', '$rootScope', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+        // You can include any angular dependencies as parameters for this function
+        // TIP: Access Route Parameters for your page via $stateParams.parameterName
+        function($scope, $stateParams, $http, $rootScope) {
+            $rootScope.list = [];
+            $rootScope.ttlcat = 0;
+            $rootScope.ttlfat = 0;
+            $rootScope.ttlch = 0;
+            $rootScope.ttlpt = 0;
+            $scope.heading = "Heading";
+
+            $scope.TakePics = function() {
+                
+                CameraPreview.takePicture(async function(imgData){
+                    $scope.heading = "Processing";
+                   
+                    link = await $http(imgUrlReq(imgData)); //pass image to the link api 
+
+                    res = await $http(cvApiReq2(link.data)); //pass image to vision api 
+                    
+                    desc = res.data.Predictions[0].Probability.toFixed(2) > 0.5 ? res.data.Predictions[0].Tag +" (" + res.data.Predictions[0].Probability.toFixed(2) * 100 + "%)" : "Generic";
+                    //desc = res.data.description.captions[0].text;
+//                    tags = res.data.tags;
+//                    
+//                    tagString = "";
+//                    for (i in tags) {
+//                        tagString += tags[i].name + " ";
+//                    }
+
+                    $scope.heading = desc; 
+                    $scope.$apply();
+                    
+                    cat = 10;
+                    fat = 10;
+                    ch = 10;
+                    pt = 10;
+                    
+                    food = getMatch(desc);
+                    
+                    var obj = {
+                        "thumbnail": String(imgData),
+                        "name": food.name,
+                        "cat": food.cal,
+                        "fat": food.fat,
+                        "ch": food.carb,
+                        "pt": food.pro
+                    };
+                    
+                    $rootScope.ttlcat += obj.cat;
+                    $rootScope.ttlfat += obj.fat;
+                    $rootScope.ttlch += obj.ch;
+                    $rootScope.ttlpt += obj.pt;
+                    $rootScope.list.push(obj);
+                    
+                    $rootscope.$digest();
+                    
+                });
+                                                      
+                
+            };
+
+        }
+    ])
+
+    .controller('nutritionCtrl', ['$scope', '$stateParams', '$rootScope', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+        // You can include any angular dependencies as parameters for this function
+        // TIP: Access Route Parameters for your page via $stateParams.parameterName
+        function($scope, $stateParams, $rootScope) {
+           $scope.DeleteItem = function(index) {
+               $rootScope.list.splice(index,1);
+           }
+
+        }
+    ])
+
+    .controller('exerciseCtrl', ['$scope', '$stateParams', '$rootScope', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+        // You can include any angular dependencies as parameters for this function
+        // TIP: Access Route Parameters for your page via $stateParams.parameterName
+        function($scope, $stateParams, $rootScope) {
+
+            $scope.weight = 120;
+
+            $scope.situpTime = function(calories, weight) {
+                calories ? calories : 0;
+                return (calories / 9).toFixed(2) + " minutes";;
+            }
+
+            $scope.jogTime = function(calories, weight) {
+                return (calories / 16).toFixed(2) + " minutes";
+            }
+
+            $scope.bballTime = function(calories, weight) {
+                burnRate = 3.4 + 0.3 * ((weight - 100) / 10);
+                return (calories / burnRate).toFixed(2) + " minutes";
+            }
+
+            $scope.bikingTime = function(calories, weight) {
+                burnRate = 6 + 0.7 * ((weight - 100) / 10);
+                return (calories / burnRate).toFixed(2) + " minutes";
+            }
+
+            $scope.swimTime = function(calories, weight) {
+                burnRate = 5.3 + 0.5 * ((weight - 100) / 10);
+                return (calories / burnRate).toFixed(2) + " minutes";
+            }
+
+            $scope.boxTime = function(calories, weight) {
+                burnRate = 4.5 + 0.5 * ((weight - 100) / 10);
+                return (calories / burnRate).toFixed(2) + " minutes";
+            }
+
+            $scope.pilatesTime = function(calories, weight) {
+                burnRate = 1.9 + 0.3 * ((weight - 100) / 10);
+                return (calories / burnRate).toFixed(2) + " minutes";
+            }
+        }
+    ])
---
+++ www/www/js/routes.js
@@ -0,0 +1,68 @@
+angular.module('app.routes', ['ionicUIRouter'])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+
+      .state('tabsController.cameraTabDefaultPage', {
+    url: '/page2',
+    views: {
+      'tab1': {
+        templateUrl: 'templates/cameraTabDefaultPage.html',
+        controller: 'cameraTabDefaultPageCtrl'
+      }
+    }
+  })
+
+  /* 
+    The IonicUIRouter.js UI-Router Modification is being used for this route.
+    To navigate to this route, do NOT use a URL. Instead use one of the following:
+      1) Using the ui-sref HTML attribute:
+        ui-sref='tabsController.nutrition'
+      2) Using $state.go programatically:
+        $state.go('tabsController.nutrition');
+    This allows your app to figure out which Tab to open this page in on the fly.
+    If you're setting a Tabs default page or modifying the .otherwise for your app and
+    must use a URL, use one of the following:
+      /page1/tab1/page3
+      /page1/tab2/page3
+  */
+  .state('tabsController.nutrition', {
+    url: '/page3',
+    views: {
+      'tab1': {
+        templateUrl: 'templates/nutrition.html',
+        controller: 'nutritionCtrl'
+      },
+      'tab2': {
+        templateUrl: 'templates/nutrition.html',
+        controller: 'nutritionCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.exercise', {
+    url: '/page4',
+    views: {
+      'tab3': {
+        templateUrl: 'templates/exercise.html',
+        controller: 'exerciseCtrl'
+      }
+    }
+  })
+
+  .state('tabsController', {
+    url: '/page1',
+    templateUrl: 'templates/tabsController.html',
+    abstract:true
+  })
+
+$urlRouterProvider.otherwise('/page1/page2')
+
+
+});
